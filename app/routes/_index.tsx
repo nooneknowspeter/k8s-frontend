49729d8619796923a0586c8f69936cd4690dbd80
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-03-03 02:26:06 -0400

    feat: created function to fetch commands objects from server

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -47,6 +47,43 @@ export default function Index() {
 
   }
 
+  const getAllCommands = async () => {
+    try {
+      const request = await fetch(commandsProductionURL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await request.json();
+
+      const response = JSON.stringify(data);
+
+      console.log(data);
+
+      console.log(response);
+
+      console.log(request.status)
+
+      if (!(data == "")) {
+        setData(data)
+      }
+      else {
+        setData("empty")
+      }
+
+      if (!(request.status == 200)) {
+        setData("error")
+        throw new Error(`Could Not Fetch Data From Server: ${request.status}`)
+      }
+
+    }
+    catch (Error) {
+      console.error(Error);
+    }
+  }
+
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
